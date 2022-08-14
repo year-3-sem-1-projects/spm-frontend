1b080073564a5634e1e5d6972625b5123ebcfd6e
christancf <christanf6@gmail.com>
2022-08-14 18:16:07 +0530

    Dependencies updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#007CF0',
+        secondary: '#00DFD8',
+        'input-border': '#888888',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
