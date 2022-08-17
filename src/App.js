18fec6b3dd0cc3d7e0a168314135c319f2a5d7a3
christancf <christanf6@gmail.com>
2022-08-17 19:30:54 +0530

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/index'
+import Question from './pages/Question/index'
+import Circle from './pages/Circle/index'
+import User from './pages/User'
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>
-      App
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/question" element={<Question />} />
+      <Route path="/circle" element={<Circle />} />
+      <Route path="/profile" element={<User />} />
+    </Routes>
   )
 }
 
