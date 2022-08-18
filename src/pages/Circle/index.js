0584b6f77bbf5e39451b4df6fd958571ab839117
christancf <christanf6@gmail.com>
2022-08-19 05:28:12 +0530

    Circle index.js modified

--- src/pages/Circle/index.js
+++ src/pages/Circle/index.js
@@ -1,28 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/ui/Navbar'
 import Button from '@mui/material/Button'
 import SnackBar from '../../components/ui/SnackBar'
-import axios from '../../lib/axios'
+import { TextField, useFormControl } from '@mui/material'
+import { createTest, readTest } from '../../services/Test'
+import imageUpload from '../../utils/imageUpload'
 
-const index = () => {
+const IMG_PATH = `circle/`
+
+const Circle = () => {
+  const [data, setData] = useState()
+  const [read, setRead] = useState()
+  const [image, setImage] = useState(null)
+  const [url, setUrl] = useState(null)
+
+  useEffect(() => {}, [read])
+
   const handleClick = async () => {
-    await axios
-      .post('/api/test/add', {
-        name: 'All Might',
-      })
-      .then(res => console.log(res))
-      .catch(e => console.error(e))
+    await createTest({ name: data })
+  }
+  const handleRead = async () => {
+    const d = await readTest()
+    setRead(d)
+    console.log(read)
+  }
+  const handleImageUpload = async () => {
+    const response = await imageUpload(image, IMG_PATH)
+    console.log(response)
   }
   return (
     <div>
       <Navbar />
-      {/* <Button variant="outlined" onClick={() => handleClick()}>
-        Primary
-      </Button> */}
-      {/* <Button /> */}
-      {/* <SnackBar message={'Testing...'} /> */}
+      <TextField
+        id="standard-basic"
+        label="Standard"
+        variant="standard"
+        onInput={e => {
+          setData(e.target.value)
+        }}
+      />
+      <Button variant="outlined" onClick={() => handleClick()}>
+        Click Me
+      </Button>
+      <Button variant="outlined" onClick={() => handleRead()}>
+        Read Me
+      </Button>
+      <input
+        type="file"
+        name=""
+        id=""
+        onChange={e => {
+          setImage(e.target.files[0])
+        }}
+      />
+      <button onClick={handleImageUpload}>Upload Image</button>
+
+      {read ? read.map(r => <p>{r.name}</p>) : <p>No Names</p>}
     </div>
   )
 }
 
-export default index
+export default Circle
