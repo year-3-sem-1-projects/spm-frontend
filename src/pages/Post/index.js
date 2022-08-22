e1ada403d160178c22d8009a131b01d2fa9a5802
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2022-08-22 19:43:10 +0530

    create files

---
+++ src/pages/Post/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/ui/Navbar'
+
+const index = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default index
