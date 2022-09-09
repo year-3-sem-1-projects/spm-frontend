0d4f2bfaf745364cddac020f78e1ba45cf259dff
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2022-09-09 11:43:27 +0530

    Component ProfileSide

---
+++ src/components/ProfileSide/ProfileSide.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import LogoSearch from "../LogoSearch/LogoSearch";
+import ProfileCard from "../ProfileCard/ProfileCard";
+import "./ProfileSide.css";
+
+const ProfileSide = () => {
+	return (
+		<div className="ProfileSide">
+			<LogoSearch />
+			<ProfileCard />
+		</div>
+	);
+};
+
+export default ProfileSide;
