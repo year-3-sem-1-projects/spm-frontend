cc33d3bd6845f5369424811bdf200d742a18c07d
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2022-09-09 11:45:11 +0530

    Change App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/index'
+// import Home from './pages/Home/index'
 import Question from './pages/Question/index'
 import Circle from './pages/Circle/index'
+import Post from './pages/Post/index'
 import User from './pages/User'
+import { ModalsProvider } from '@mantine/modals'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/question" element={<Question />} />
-      <Route path="/circle" element={<Circle />} />
-      <Route path="/profile" element={<User />} />
-    </Routes>
+    <ModalsProvider>
+      <Routes>
+        <Route path="/" element={<Post />} />
+        {/* <Route path="/post" element={<Post />} /> */}
+        <Route path="/question" element={<Question />} />
+        <Route path="/circle" element={<Circle />} />
+        <Route path="/profile" element={<User />} />
+      </Routes>
+    </ModalsProvider>
   )
 }
 
