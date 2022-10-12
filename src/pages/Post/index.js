525c0c659bf70bfa0cdabef18a953d562dd719b8
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2022-10-12 16:42:16 +0530

    Add search component to index.js

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -14,6 +14,7 @@ import PostComponent from '../../components/Post/PostComponent'
 import { readPost } from '../../services/Post'
 import Loading from '../../components/Loading/Loading'
 import PostPanel from '../../components/PostPanel/PostPanel'
+import SearchPost from './SearchPost'
 
 const Index = () => {
   const [postData, setPostData] = useState([])
@@ -112,6 +113,7 @@ const Index = () => {
                     }}
                   >
                     Search
+                    <SearchPost />
                   </Typography>
                   <Box
                     sx={{
