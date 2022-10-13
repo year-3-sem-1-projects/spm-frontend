62e4911e4d3cad2e913f85fa05e6be313314cfff
christancf <christanf6@gmail.com>
2022-10-13 10:16:51 +0530

    Circle dashboard modified

--- src/pages/Circle/dashboard/index.js
+++ src/pages/Circle/dashboard/index.js
@@ -4,31 +4,154 @@ import Settings from './settings'
 import People from './people'
 import Stats from './stats'
 import Leave from './leave'
+import SideNavigation from '../../../components/SideNavigation/SideNavigation.jsx'
+import SettingsIcon from '@mui/icons-material/Settings'
+import GroupsIcon from '@mui/icons-material/Groups'
+import DonutSmallIcon from '@mui/icons-material/DonutSmall'
+import LogoutIcon from '@mui/icons-material/Logout'
+import SecurityIcon from '@mui/icons-material/Security'
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material'
 
 const Dashboard = () => {
-  const { role } = useParams()
-  if (role === 'admin') {
-    return (
-      <>
-        <Routes>
-          <Route path="settings" element={<Settings />} />
-          <Route path="people" element={<People />} />
-          <Route path="stats" element={<Stats />} />
-          <Route path="leave" element={<Leave />} />
-        </Routes>
-      </>
-    )
-  }
-  if (role === 'member') {
-    return (
-      <>
-        <Routes>
-          <Route path="people" element={<People />} />
-          <Route path="leave" element={<Leave />} />
-        </Routes>
-      </>
-    )
-  }
+  const { role, name } = useParams()
+  const BASE_PATH = `/circle/${name}/${role}/dashboard`
+
+  const items = [
+    {
+      name: 'Settings',
+      icon: <SettingsIcon />,
+      link: `${BASE_PATH}/settings`,
+    },
+    {
+      name: 'People',
+      icon: <GroupsIcon />,
+      link: `${BASE_PATH}/people`,
+    },
+    {
+      name: 'Stats',
+      icon: <DonutSmallIcon />,
+      link: `${BASE_PATH}/stats`,
+    },
+    {
+      name: 'Leave Circle',
+      icon: <LogoutIcon />,
+      link: `${BASE_PATH}/leave`,
+    },
+  ]
+
+  // if (role === 'admin') {
+  return (
+    <>
+      <Box>
+        <TopBar name={name} role={role} />
+      </Box>
+      <Routes>
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/leave" element={<Leave />} />
+      </Routes>
+    </>
+  )
 }
+
+const TopBar = ({ name, role }) => {
+  return (
+    <Paper
+      elevation={0}
+      sx={{
+        // width: '100vw',
+        p: 2,
+        backgroundColor: '#F5F5F5',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <Typography
+        variant="h5"
+        sx={{
+          fontWeight: 'bold',
+          marginLeft: '15vw',
+        }}
+      >
+        {name}
+      </Typography>
+      <Button
+        variant="outlined"
+        startIcon={<SecurityIcon />}
+        sx={{
+          position: 'absolute',
+          right: '24rem',
+          backgroundColor: '#fff',
+          color: '#000',
+          fontWeight: 'bold',
+          textTransform: 'none',
+          borderRadius: '20px',
+          boxShadow: '0px 0px 10px rgba(0,0,0,0.5)',
+          '&:hover': {
+            backgroundColor: '#000',
+            color: '#fff',
+          },
+        }}
+        onClick={event => console.log(event.currentTarget)}
+      >
+        {role}
+      </Button>
+    </Paper>
+  )
+}
+
+// <Container>
+//   <Grid
+//     container
+//     columnSpacing={{ xs: 1, sm: 2, md: 4, lg: 6 }}
+//     sx={{
+//       marginTop: '50px',
+//     }}
+//   >
+//     <Grid item md={3} zeroMinWidth>
+//       <Grid
+//         item
+//         sx={{
+//           marginBottom: '40px',
+//         }}
+//       >
+//         <Paper className={`p-5`}>
+//           <Typography
+//             variant="h6"
+//             sx={{
+//               fontWeight: 'bold',
+//             }}
+//           >
+//             {`${
+//               role.substring(0, 1).toUpperCase() + role.substring(1)
+//             } Dashboard`}
+//           </Typography>
+//         </Paper>
+//       </Grid>
+//       <Grid item className={`pb-10`} zeroMinWidth>
+//         <SideNavigation items={items} />
+//       </Grid>
+//     </Grid>
+//     <Grid item md={6}>
+//       <Grid
+//         item
+//         sx={{
+//           marginBottom: '40px',
+//         }}
+//       >
+//         Settings
+//       </Grid>
+//     </Grid>
+//   </Grid>
+// </Container>
 
 export default Dashboard
--- src/pages/Circle/dashboard/settings.js
+++ src/pages/Circle/dashboard/settings.js
@@ -1,20 +1,5 @@
-import {
-  Box,
-  Button,
-  Container,
-  Grid,
-  Paper,
-  TextField,
-  Typography,
-} from '@mui/material'
 import React, { useState } from 'react'
-import SideNavigation from '../../../components/SideNavigation/SideNavigation.jsx'
-import SettingsIcon from '@mui/icons-material/Settings'
-import GroupsIcon from '@mui/icons-material/Groups'
-import DonutSmallIcon from '@mui/icons-material/DonutSmall'
-import LogoutIcon from '@mui/icons-material/Logout'
 import { useParams } from 'react-router-dom'
-import SecurityIcon from '@mui/icons-material/Security'
 import GetCurrentUser from '../../../hooks/getCurrentUser.js'
 import {
   getAllCircles,
@@ -22,13 +7,13 @@ import {
   updateCircle,
 } from '../../../services/Circle.js'
 import { useEffect } from 'react'
+import { Box, Button, Paper, TextField, Typography } from '@mui/material'
 
 const Settings = () => {
   const { role, name } = useParams()
   const [circlesData, setCirclesData] = useState([])
   const [circleData, setCircleData] = useState({})
   const [circleName, setCircleName] = useState()
-
   const [circleDescription, setCircleDescription] = useState('')
   const [circleImage, setCircleImage] = useState('')
   const [circleCoverImage, setCircleCoverImage] = useState('')
@@ -38,7 +23,6 @@ const Settings = () => {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
   const user = GetCurrentUser()
-  // const circleData = getCircle(name)
 
   useEffect(() => {
     getAllCircles()
@@ -65,7 +49,7 @@ const Settings = () => {
         setCircleData({})
         setCirclesData([])
       })
-  }, [])
+  }, [name])
 
   async function handleSubmit(e) {
     e.preventDefault()
@@ -100,203 +84,77 @@ const Settings = () => {
     }
   }
 
-  const items = [
-    {
-      name: 'Settings',
-      icon: <SettingsIcon />,
-      link: 'settings',
-    },
-    {
-      name: 'People',
-      icon: <GroupsIcon />,
-      link: 'members',
-    },
-    {
-      name: 'Stats',
-      icon: <DonutSmallIcon />,
-      link: 'stats',
-    },
-    {
-      name: 'Leave Circle',
-      icon: <LogoutIcon />,
-      link: 'leave',
-    },
-  ]
-
   return (
     <>
-      <Box>
-        <Paper
-          elevation={0}
+      <Paper className={`p-5`}>
+        <Typography
+          variant="h6"
           sx={{
-            // width: '100vw',
-            p: 2,
-            backgroundColor: '#F5F5F5',
-            display: 'flex',
-            alignItems: 'center',
+            fontWeight: 'bold',
           }}
         >
-          <Typography
-            variant="h5"
-            sx={{
-              fontWeight: 'bold',
-              marginLeft: '15vw',
-            }}
-          >
-            {circleData.name}
-          </Typography>
-          <Button
-            variant="outlined"
-            startIcon={<SecurityIcon />}
-            sx={{
-              position: 'absolute',
-              right: '24rem',
-              backgroundColor: '#fff',
-              color: '#000',
-              fontWeight: 'bold',
-              textTransform: 'none',
-              borderRadius: '20px',
-              boxShadow: '0px 0px 10px rgba(0,0,0,0.5)',
-              '&:hover': {
-                backgroundColor: '#000',
-                color: '#fff',
-              },
-            }}
-            onClick={event => console.log(event.currentTarget)}
-          >
-            {role}
-          </Button>
-        </Paper>
-      </Box>
-      <Container>
-        <Grid
-          container
-          columnSpacing={{ xs: 1, sm: 2, md: 4, lg: 6 }}
+          General
+        </Typography>
+        <Box
           sx={{
-            marginTop: '50px',
+            marginTop: '30px',
           }}
         >
-          <Grid item md={3} zeroMinWidth>
-            <Grid
-              item
+          <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+            <TextField
+              id="outlined-basic"
+              label="Circle Name"
+              variant="outlined"
+              fullWidth
+              onInput={e => handleOnInputName(e.target.value)}
+              value={circleName}
+              onChange={event => setCircleName(event.target.value)}
+              error={circleNameError}
+              helperText={
+                circleNameError
+                  ? 'Circle name should have atleast 5 characters'
+                  : circleNameExistsError
+                  ? 'Circle name already exists'
+                  : ''
+              }
               sx={{
-                marginBottom: '40px',
+                marginBottom: '20px',
+              }}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Description"
+              variant="outlined"
+              fullWidth
+              value={circleDescription}
+              onChange={event => setCircleDescription(event.target.value)}
+              sx={{
+                marginBottom: '20px',
+              }}
+            />
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'flex-end',
               }}
             >
-              <Paper className={`p-5`}>
-                <Typography
-                  variant="h6"
-                  sx={{
-                    fontWeight: 'bold',
-                  }}
-                >
-                  {`${
-                    role.substring(0, 1).toUpperCase() + role.substring(1)
-                  } Dashboard`}
-                </Typography>
-              </Paper>
-            </Grid>
-            <Grid item className={`pb-10`} zeroMinWidth>
-              <SideNavigation menuItems={items} />
-            </Grid>
-          </Grid>
-          <Grid item md={6}>
-            <Grid
-              item
-              sx={{
-                marginBottom: '40px',
-              }}
-            >
-              <Paper className={`p-5`}>
-                <Typography
-                  variant="h6"
-                  sx={{
-                    fontWeight: 'bold',
-                  }}
-                >
-                  General
-                </Typography>
-                <Box
-                  sx={{
-                    marginTop: '30px',
-                  }}
-                >
-                  <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-                    <TextField
-                      id="outlined-basic"
-                      label="Circle Name"
-                      variant="outlined"
-                      fullWidth
-                      onInput={e => handleOnInputName(e.target.value)}
-                      value={circleName}
-                      onChange={event => setCircleName(event.target.value)}
-                      error={circleNameError}
-                      helperText={
-                        circleNameError
-                          ? 'Circle name should have atleast 5 characters'
-                          : circleNameExistsError
-                          ? 'Circle name already exists'
-                          : ''
-                      }
-                      sx={{
-                        marginBottom: '20px',
-                      }}
-                    />
-                    <TextField
-                      id="outlined-basic"
-                      label="Description"
-                      variant="outlined"
-                      fullWidth
-                      value={circleDescription}
-                      onChange={event =>
-                        setCircleDescription(event.target.value)
-                      }
-                      sx={{
-                        marginBottom: '20px',
-                      }}
-                    />
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        justifyContent: 'flex-end',
-                      }}
-                    >
-                      <Button variant="text" type="reset">
-                        Reset
-                      </Button>
-                      <Button
-                        autoFocus
-                        onClick={handleSubmit}
-                        variant="contained"
-                        sx={{
-                          marginLeft: '10px',
-                        }}
-                      >
-                        Update
-                      </Button>
-                    </Box>
-                  </form>
-                </Box>
-              </Paper>
-            </Grid>
-            {/* <Grid item zeroMinWidth>
-              <Paper className={`p-5`}>
-                <Typography variant="h5">Center</Typography>
-              </Paper>
-            </Grid>
-            <Grid item zeroMinWidth>
-              <Paper className={`p-5`}>
-                <Typography variant="h5">Center</Typography>
-              </Paper>
-            </Grid>
-            <Grid item zeroMinWidth>
-              <Paper className={`p-5`}>
-                <Typography variant="h5">Center</Typography>
-              </Paper> 
-            </Grid>*/}
-          </Grid>
-        </Grid>
-      </Container>
+              <Button variant="text" type="reset">
+                Reset
+              </Button>
+              <Button
+                autoFocus
+                onClick={handleSubmit}
+                variant="contained"
+                sx={{
+                  marginLeft: '10px',
+                }}
+              >
+                Update
+              </Button>
+            </Box>
+          </form>
+        </Box>
+      </Paper>
     </>
   )
 }
