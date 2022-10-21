854c1724fe22e38053bc4e04e11009c8066177a8
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2022-10-21 11:50:54 +0530

    Add post panel to create post modal

--- src/components/PostPanel/PostPanel.js
+++ src/components/PostPanel/PostPanel.js
@@ -1,14 +1,27 @@
+import * as React from 'react'
+import { useState } from 'react'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 import CreateIcon from '@mui/icons-material/Create'
 import RateReviewTwoToneIcon from '@mui/icons-material/RateReviewTwoTone'
 import Card from '@mui/joy/Card'
 import GetCurrentUser from '../../hooks/getCurrentUser'
+import AddPostDialog from '../../components/Post/AddPostDialog'
 
 const PostPanel = () => {
-  const user = GetCurrentUser();
+  const user = GetCurrentUser()
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen)
+  }
   console.log('user', user)
   return (
     <>
+      <AddPostDialog
+        isDialogOpened={isOpen}
+        handleCloseDialog={() => setIsOpen(false)}
+      />
       <Card
         elevation={3}
         sx={{
@@ -18,21 +31,21 @@ const PostPanel = () => {
           backgroundColor: '#FFFFFF',
         }}
       >
-        
         <div className="w-full h-full">
           <div className="flex justify-evenly items-center p-5">
-            {user ? 
-            <img
-              src={user.photo_url}
-              alt="Profile"
-              className="rounded-full h-10 w-10"
-            /> : 
-            <img
-              src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
-              alt=""
-              className="rounded-full h-10 w-10"
-            />
-            }
+            {user ? (
+              <img
+                src={user.photo_url}
+                alt="Profile"
+                className="rounded-full h-10 w-10"
+              />
+            ) : (
+              <img
+                src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+                alt=""
+                className="rounded-full h-10 w-10"
+              />
+            )}
 
             <input
               className="w-[30rem] h-7 bg-white-400 rounded-md outline-fill p-4"
@@ -52,7 +65,7 @@ const PostPanel = () => {
               </div>
               <div className="h-6 w-0.5 bg-black"></div>
               <div className="flex">
-                <CreateIcon />
+                <CreateIcon onClick={() => handleOpen()} />
                 <span>Post</span>
               </div>
             </div>
