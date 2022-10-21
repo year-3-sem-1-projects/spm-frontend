cd4b5c9b6b38f09d36e7cabaf4a21c9f29a5db55
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2022-10-21 11:50:27 +0530

    Add view post

---
+++ src/components/Post/ViewPost.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { CardActionArea } from '@mui/material'
+import DialogContent from '@mui/material/DialogContent'
+import DialogTitle from '@mui/material/DialogTitle'
+import Dialog from '@mui/material/Dialog'
+
+export default function ViewPost({ isDialogOpened, handleCloseDialog, data }) {
+  console.log('dataaaaaa', data)
+  const [fullWidth] = useState(true)
+  const [maxWidth] = useState('sm')
+
+  const handleClose = () => {
+    handleCloseDialog(false)
+  }
+
+  return (
+    <React.Fragment>
+      <Dialog
+        fullWidth={fullWidth}
+        maxWidth={maxWidth}
+        open={isDialogOpened}
+        onClose={handleClose}
+        aria-labelledby="max-width-dialog-title"
+      >
+        <DialogTitle id="max-width-dialog-title">View Post</DialogTitle>
+        <DialogContent>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image="https://source.unsplash.com/random"
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  ggggggggggggggg
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  ggggggggggggggg
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </DialogContent>
+      </Dialog>
+    </React.Fragment>
+  )
+}
